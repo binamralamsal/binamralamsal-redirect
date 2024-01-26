45dc961bcc9ab735ebb73bd160fff9a30669cabb
Binamra Lamsal <binamra@skiff.com>
2024-01-26 23:10:53 +0545

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://github.com/binamralamsal",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
