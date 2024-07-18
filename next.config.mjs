6989474624a904b132930875bd7b096bbd280d85
Binamra Lamsal <61900781+binamralamsal@users.noreply.github.com>
2024-07-19 00:35:37 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "https://github.com/binamralamsal",
-        permanent: true,
-      },
-    ];
-  },
+ 
 };
 
 export default nextConfig;
